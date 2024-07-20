68633fabdb6e6e16aebcc11678cd60af56227170
Mark Lawrence <mark.j.lawrence@ibm.com>
2024-07-20 23:28:30 +0100

    docs: react app

---
+++ docs/src/app/how/page.js
@@ -0,0 +1,5 @@
+`use client`;
+
+export default function HowPage() {
+  return <div>HOW PAGE</div>;
+}
---
+++ docs/src/app/layout.js
@@ -0,0 +1,17 @@
+import './globals.scss';
+import { Providers } from './providers';
+
+export const metadata = {
+  title: 'IBM CICS - Security Definition Validation',
+  description: 'IBM CICS SDV documentation',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
