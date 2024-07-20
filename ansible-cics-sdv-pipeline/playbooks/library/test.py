3cd7d7b3de963f45fe5796dc1272995a7a46b04e
Mark Lawrence <mark.j.lawrence@ibm.com>
2024-07-20 17:38:21 +0100

    feat: other code

---
+++ ansible-cics-sdv-pipeline/playbooks/library/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+'''
+stuff
+'''
+
+
+def test(test):
+    return test
+
+
+def main():
+    '''The entry point for the module'''
+    test('blah')
+
+
+if __name__ == '__main__':
+    main()
